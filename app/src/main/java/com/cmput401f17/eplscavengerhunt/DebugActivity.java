8a6969637c68cbeeb63fd30f88cfbd4914687856
Ryan Perez <rperez@ualberta.ca>
2017-10-02 13:21:28 -0600

    Created blank activities of the game's main views. Debug screen as a hub to access any of them.

--- app/src/main/java/com/cmput401f17/eplscavengerhunt/DebugActivity.java
+++ app/src/main/java/com/cmput401f17/eplscavengerhunt/DebugActivity.java
@@ -1,7 +1,11 @@
 package com.cmput401f17.eplscavengerhunt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class DebugActivity extends AppCompatActivity {
 
@@ -9,5 +13,44 @@ public class DebugActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debug);
+
+        Button goto_QuestionActivity = (Button) findViewById(R.id.goto_questionactivity_button);
+        Button goto_LocationActivity = (Button) findViewById(R.id.goto_locationactivity_button);
+        Button goto_congratulationsActivity = (Button) findViewById(R.id.goto_congratulationsactivity_button);
+        Button goto_resultsActivity = (Button) findViewById(R.id.goto_resultsactivity_button);
+
+        goto_QuestionActivity.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("DebugActivity", "going to QuestionActivity");
+                Intent intent = new Intent(DebugActivity.this, QuestionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        goto_LocationActivity.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("DebugActivity", "going to LocationActivity");
+                Intent intent = new Intent(DebugActivity.this, LocationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        goto_congratulationsActivity.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("DebugActivity", "going to CongratulationsActivity");
+                Intent intent = new Intent(DebugActivity.this, CongratulationsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        goto_resultsActivity.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("DebugActivity", "going to ResultsActivity");
+                Intent intent = new Intent(DebugActivity.this, ResultsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/cmput401f17/eplscavengerhunt/QuestionActivity.java
@@ -0,0 +1,13 @@
+package com.cmput401f17.eplscavengerhunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
