3d5ef2249fddf0e659b9271945c4e492521eb4b3
Jennifer Yuen <jennyuen@Jennifers-MacBook-Pro.local>
2017-10-02 20:26:17 -0600

    comment out estimo (conflicts with dagger)

--- app/src/main/java/com/cmput401f17/eplscavengerhunt/dagger/AppComponent.java
+++ app/src/main/java/com/cmput401f17/eplscavengerhunt/dagger/AppComponent.java
@@ -1,6 +1,11 @@
 package com.cmput401f17.eplscavengerhunt.dagger;
 
+import com.cmput401f17.eplscavengerhunt.activity.CongratulationsActivity;
 import com.cmput401f17.eplscavengerhunt.activity.DebugActivity;
+import com.cmput401f17.eplscavengerhunt.activity.LocationActivity;
+import com.cmput401f17.eplscavengerhunt.activity.QuestionActivity;
+import com.cmput401f17.eplscavengerhunt.activity.ResultsActivity;
+import com.cmput401f17.eplscavengerhunt.activity.TitleActivity;
 
 import javax.inject.Singleton;
 
@@ -9,5 +14,10 @@ import dagger.Component;
 @Singleton
 @Component(modules = AppModule.class)
 public interface AppComponent {
+    void inject(CongratulationsActivity congratulationsActivity);
     void inject(DebugActivity debugActivity);
+    void inject(LocationActivity locationActivity);
+    void inject(QuestionActivity questionActivity);
+    void inject(ResultsActivity resultsActivity);
+    void inject(TitleActivity titleActivity);
 }
