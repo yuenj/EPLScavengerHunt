6c767455b627f3e4e5681743690322ec536d08c6
Jennifer Yuen <jennyuen@Jennifers-MacBook-Pro.local>
2017-10-02 18:50:59 -0600

    integrate dagger 2

--- app/src/main/java/com/cmput401f17/eplscavengerhunt/activity/DebugActivity.java
+++ app/src/main/java/com/cmput401f17/eplscavengerhunt/activity/DebugActivity.java
@@ -1,4 +1,4 @@
-package com.cmput401f17.eplscavengerhunt;
+package com.cmput401f17.eplscavengerhunt.activity;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -6,18 +6,30 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.cmput401f17.eplscavengerhunt.R;
+import com.cmput401f17.eplscavengerhunt.ScavengerHuntApplication;
+import com.cmput401f17.eplscavengerhunt.controller.GameController;
+
+import javax.inject.Inject;
 
 public class DebugActivity extends AppCompatActivity {
+
+    @Inject
+    GameController gameController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debug);
+        ScavengerHuntApplication.getInstance().getAppComponent().inject(this);
 
         Button goto_QuestionActivity = (Button) findViewById(R.id.goto_questionactivity_button);
         Button goto_LocationActivity = (Button) findViewById(R.id.goto_locationactivity_button);
         Button goto_congratulationsActivity = (Button) findViewById(R.id.goto_congratulationsactivity_button);
         Button goto_resultsActivity = (Button) findViewById(R.id.goto_resultsactivity_button);
+        TextView dagger = (TextView) findViewById(R.id.dagger);
 
         goto_QuestionActivity.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -51,6 +63,6 @@ public class DebugActivity extends AppCompatActivity {
             }
         });
 
-
+        dagger.setText(gameController.initGame());
     }
 }
--- app/src/main/java/com/cmput401f17/eplscavengerhunt/activity/TitleActivity.java
+++ app/src/main/java/com/cmput401f17/eplscavengerhunt/activity/TitleActivity.java
@@ -1,4 +1,4 @@
-package com.cmput401f17.eplscavengerhunt;
+package com.cmput401f17.eplscavengerhunt.activity;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -7,7 +7,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.cmput401f17.eplscavengerhunt.R;
+
 public class TitleActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
