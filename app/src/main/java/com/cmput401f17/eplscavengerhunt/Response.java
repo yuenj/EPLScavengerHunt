da2eeb8fc9bef2321f3fdc463461f5af0cee1e62
Ryan Perez <rperez@ualberta.ca>
2017-10-03 17:23:15 -0600

    added first version of questions and responses, along with complete tests.

---
+++ app/src/main/java/com/cmput401f17/eplscavengerhunt/Response.java
@@ -0,0 +1,42 @@
+package com.cmput401f17.eplscavengerhunt;
+
+/**
+ * Created by ryan on 2017-10-03.
+ */
+
+public class Response {
+    private String responseStr;
+    private Boolean correct;
+
+
+    public Response() {
+        this.responseStr = "";
+        this.correct = true;
+    }
+
+    public Response(String responseStr) {
+        this.responseStr = responseStr;
+        this.correct = true;
+    }
+
+    public String getResponseStr() {
+        return responseStr;
+    }
+
+    public void setResponseStr(String responseStr) {
+        this.responseStr = responseStr;
+    }
+
+    public Boolean isCorrect() {
+        return correct;
+    }
+
+    public void markCorrect() {
+        this.correct = true;
+
+    }
+
+    public void markIncorrect() {
+        this.correct = false;
+    }
+}
