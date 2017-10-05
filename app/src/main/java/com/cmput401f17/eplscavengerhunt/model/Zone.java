75af01580a4d82e4a9155606a26dc19c03a64444
alexczeto <alexczeto@yahoo.ca>
2017-10-05 15:40:26 -0600

    Added things to zone model class

--- app/src/main/java/com/cmput401f17/eplscavengerhunt/model/Zone.java
+++ app/src/main/java/com/cmput401f17/eplscavengerhunt/model/Zone.java
@@ -1,8 +1,18 @@
 package com.cmput401f17.eplscavengerhunt.model;
 
 /**
- * Created by czeto on 05/10/17.
+ * Beacon ids are specific to each estimote beacon and are set by estimote.
+ * You need to go to estimote.cloud.com to see a given beacons ID.
  */
 
 public class Zone {
+    private String beaconID;
+
+    public Zone(String beaconID) {
+        this.beaconID = beaconID;
+    }
+
+    public String getBeaconID() {
+        return beaconID;
+    }
 }
