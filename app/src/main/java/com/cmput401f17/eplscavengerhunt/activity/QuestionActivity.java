7ac9ef5b265517fdc634b3cd9c7409749548e5cd
will1029 <william-qw@hotmail.com>
2017-10-08 22:38:41 -0600

    Implemted Question Activity and Controller
    -Zones are displayed
    -Question Prompt displayed
    -Basic MC functionality
    -Basic Controller functionality
    -Tested and confirmed all functions work

--- app/src/main/java/com/cmput401f17/eplscavengerhunt/activity/QuestionActivity.java
+++ app/src/main/java/com/cmput401f17/eplscavengerhunt/activity/QuestionActivity.java
@@ -1,48 +1,106 @@
 package com.cmput401f17.eplscavengerhunt.activity;
 
 /* Imports  */
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cmput401f17.eplscavengerhunt.R;
+import com.cmput401f17.eplscavengerhunt.model.MultChoiceQuestion;
+import com.cmput401f17.eplscavengerhunt.model.Question;
+import com.cmput401f17.eplscavengerhunt.controller.QuestionController;
+
 
 import java.util.ArrayList;
 
 public class QuestionActivity extends AppCompatActivity {
 
+    private QuestionController qController;
+    private Question currentQuestion;
+
+    public QuestionActivity() {
+        qController = new QuestionController();
+        currentQuestion = new Question();
+    }
+
+    /**
+     * Displays the current zone
+     */
+    private void displayZone() {
+        TextView zone = (TextView)findViewById(R.id.zone);
+        zone.setText("Zone: " + qController.requestZone());
+        zone.setTextColor(Color.RED);
+    }
+
+    /**
+     * Displays the current question prompt
+     */
+    private void displayPrompt() {
+        TextView prompt = (TextView)findViewById(R.id.question_prompt);
+        prompt.setText("Task: " + currentQuestion.getQuestionPrompt());
+    }
+
+    private void displayMultChoice(){
+        setContentView(R.layout.activity_mult_choice);
+
+        displayZone();
+        displayPrompt();
+
+        /* Get the MC choices */
+         ArrayList<String> choices = new ArrayList<String>();
+         choices.add("Hello");
+         choices.add("World");
+         choices.add("!");
+
+        /* Create choice button(s) */
+         for(int i = 0; i < choices.size(); i++) {
+             Button mcOption = new Button(this);
+             mcOption.setText(choices.get(i));
+
+             LinearLayoutCompat layout = (LinearLayoutCompat) findViewById(R.id.mult_question_layout);
+             LinearLayoutCompat.LayoutParams parameters = new LinearLayoutCompat.LayoutParams(LinearLayoutCompat.LayoutParams.MATCH_PARENT, LinearLayoutCompat.LayoutParams.WRAP_CONTENT);
+             layout.addView(mcOption, parameters);
+
+            /* Listen for button click. If clicked, make a toast telling which button was clicked */
+             final int id = i;
+             mcOption.setOnClickListener(new View.OnClickListener() {
+                 public void onClick(View view) {
+                     Toast.makeText(view.getContext(), "Button clicked index = " + id, Toast.LENGTH_SHORT).show();
+
+                     //TODO: Pass response to Question Controller
+                 }
+             });
+         }
+    }
+
+    private void displayWrittenInput(){
+        setContentView(R.layout.activity_written_input);
+
+        displayZone();
+        displayPrompt();
+    }
+
+    private void displayPicInput(){
+        setContentView(R.layout.activity_pic_input);
+
+        displayZone();
+        displayPrompt();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mult_choice);
 
-        QuestionController.requestQuestion()
+        currentQuestion = qController.requestQuestion();
 
-        /* Get the MC choices */
-        choices.add("text");
-        choices.add("Ryan");
+        displayMultChoice();
+        //displayWrittenInput();
+        //displayPicInput();
 
-        /* Create choice button(s) */
-        for(int i = 0; i < choices.size(); i++) {
-            Button mcOption = new Button(this);
-            mcOption.setText(choices.get(i));
-
-            LinearLayoutCompat layout = (LinearLayoutCompat) findViewById(R.id.question_layout);
-            LinearLayoutCompat.LayoutParams parameters = new LinearLayoutCompat.LayoutParams(LinearLayoutCompat.LayoutParams.MATCH_PARENT, LinearLayoutCompat.LayoutParams.WRAP_CONTENT);
-            layout.addView(mcOption, parameters);
-
-            /* Listen for button click. If clicked, make a toast telling which button was clicked */
-            final int id = i;
-            mcOption.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    Toast.makeText(view.getContext(), "Button clicked index = " + id, Toast.LENGTH_SHORT).show();
-
-                    //TODO: Pass response to Question Controller
-                }
-            });
-        }
     }
 }
--- app/src/main/java/com/cmput401f17/eplscavengerhunt/controller/QuestionController.java
+++ app/src/main/java/com/cmput401f17/eplscavengerhunt/controller/QuestionController.java
@@ -1,27 +1,48 @@
 package com.cmput401f17.eplscavengerhunt.controller;
 
 import com.cmput401f17.eplscavengerhunt.model.Question;
+import com.cmput401f17.eplscavengerhunt.model.ScavHuntState;
+import com.cmput401f17.eplscavengerhunt.model.Zone;
 
-class QuestionController {
-    private static final QuestionController ourInstance = new QuestionController();
+/**
+ * An intermediate class that passes data
+ */
+public class QuestionController {
 
-    static QuestionController getInstance() {
-        return ourInstance;
+    private ScavHuntState huntState;
+
+    public QuestionController() {
+            huntState = new ScavHuntState();
     }
 
-    private QuestionController() {}
-
-    public void requestQuestion() {
-
-        //TODO Determine view
-        //getCurrentStage();
-        //getQuestion(state);
-
-
+    /**
+     * Gets the data relating to the current question
+     * @return A Question object relating to the current question
+     */
+    public Question requestQuestion() {
+        Question question = new Question(1, "Test", "TestAnswer");
+        return(question);
+        //return(huntState.getCurrentQuestion());
     }
 
+    /**
+     * Gets the current zone's name
+     * @return A string representing the Zone
+     */
+    public String requestZone() {
+        Zone zone = new Zone("Test");
+        zone.setName("1");
+
+        return(zone.getName());
+        //return(huntState.getCurrentZone().getName());
+    }
+
+    /**
+     *  Passes the user answer
+     *  @pre User has input an answer
+     */
     public void requestSubmitResponse(){
-
+        //huntState.addResponse();
     }
 
     public void skip(Question question){
