8da272037f791d88b5898fbf0908793f5fffd0f0
Jennifer Yuen <jennyuen@Jennifers-MacBook-Pro.local>
2017-10-09 18:26:59 -0600

    Create results adapter test

---
+++ app/src/test/java/com/cmput401f17/eplscavengerhunt/custom/ResultsAdapterTest.java
@@ -0,0 +1,11 @@
+package com.cmput401f17.eplscavengerhunt.custom;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ResultsAdapterTest {
+
+}
