eb5bfa03269e3c9f5e5d389ca73886d9962adb02
alexczeto <alexczeto@yahoo.ca>
2017-10-15 15:05:05 -0600

    Created empty rules, about, and credits activity

---
+++ app/src/main/java/com/cmput401f17/eplscavengerhunt/activity/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.cmput401f17.eplscavengerhunt.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cmput401f17.eplscavengerhunt.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
