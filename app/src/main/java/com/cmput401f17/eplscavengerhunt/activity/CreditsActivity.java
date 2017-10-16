5de7f38a9890bf2e388ebac9a294505692e5fb6d
alexczeto <alexczeto@yahoo.ca>
2017-10-16 15:49:04 -0600

    Temporary Look for rules,about, and credits.

--- app/src/main/java/com/cmput401f17/eplscavengerhunt/activity/CreditsActivity.java
+++ app/src/main/java/com/cmput401f17/eplscavengerhunt/activity/CreditsActivity.java
@@ -1,7 +1,11 @@
 package com.cmput401f17.eplscavengerhunt.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.cmput401f17.eplscavengerhunt.R;
 
@@ -11,5 +15,13 @@ public class CreditsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+        Button returnButton = (Button) findViewById(R.id.credits_return_button);
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(CreditsActivity.this, TitleActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
