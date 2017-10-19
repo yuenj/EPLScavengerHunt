5ab506cdaa14d79e33f9af2730fd52ce3b11628f
Jennifer Yuen <jennyuen@Jennifers-MacBook-Pro.local>
2017-10-19 17:49:23 -0600

    Add congratulations activity test

--- app/src/androidTest/java/com/cmput401f17/eplscavengerhunt/CongratulationsActivityTest.java
+++ app/src/androidTest/java/com/cmput401f17/eplscavengerhunt/CongratulationsActivityTest.java
@@ -1,8 +1,40 @@
 package com.cmput401f17.eplscavengerhunt;
 
-/**
- * Created by jennyuen on 2017-10-19.
- */
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
 
-public class CongratulationsActivityTest {
+import com.cmput401f17.eplscavengerhunt.activity.CongratulationsActivity;
+import com.cmput401f17.eplscavengerhunt.activity.SummaryActivity;
+import com.robotium.solo.Solo;
+
+public class CongratulationsActivityTest extends
+        ActivityInstrumentationTestCase2<CongratulationsActivity> {
+    private static final String ERR_MSG = "Wrong Activity";
+
+    private Solo solo;
+
+    public CongratulationsActivityTest() {
+        super(CongratulationsActivity.class);
+    }
+
+    public void setUp() {
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void testCongratulationsActivityIsProperlyDisplayed() {
+        solo.assertCurrentActivity(ERR_MSG, CongratulationsActivity.class);
+        TextView congratsHeader = solo.getCurrentActivity().findViewById(R.id.title);
+        assertEquals(congratsHeader.getText().toString(), solo.getString(R.string.congrats_msg_header));
+        TextView congratsBody = solo.getCurrentActivity().findViewById(R.id.CongratsMessage);
+        assertEquals(congratsBody.getText().toString(), solo.getString(R.string.congrats_msg_body));
+    }
+
+    public void testSummaryButtonShouldGoToSummaryActivity() {
+        solo.clickOnText(solo.getString(R.string.goto_summary_activity_text));
+        solo.assertCurrentActivity(ERR_MSG, SummaryActivity.class);
+    }
+
+    public void tearDown() {
+        solo.finishOpenedActivities();
+    }
 }
