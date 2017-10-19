b513979afe2fd80c84d668b5dd1af3fd72771da1
yuenj <yuenj@users.noreply.github.com>
2017-10-19 14:22:51 -0600

    Title Activity Instrumentation Test (#114)
    * Add title activity test
    
    * add robotium and organize dependencies
    
    * enable multi dex for instrumentation testing
    
    * add title activity tests
    
    * move private static variables before private variables
    
    * remove unnecessary import

---
+++ app/src/androidTest/java/com/cmput401f17/eplscavengerhunt/TitleActivityTest.java
@@ -0,0 +1,55 @@
+package com.cmput401f17.eplscavengerhunt;
+
+import android.content.res.Resources;
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.cmput401f17.eplscavengerhunt.activity.AboutActivity;
+import com.cmput401f17.eplscavengerhunt.activity.CreditsActivity;
+import com.cmput401f17.eplscavengerhunt.activity.DebugActivity;
+import com.cmput401f17.eplscavengerhunt.activity.RulesActivity;
+import com.cmput401f17.eplscavengerhunt.activity.TitleActivity;
+import com.robotium.solo.Solo;
+
+public class TitleActivityTest extends ActivityInstrumentationTestCase2<TitleActivity> {
+    private static final String errMsg = "Wrong Activity";
+
+    private Solo solo;
+    private Resources res;
+
+    public TitleActivityTest() {
+        super(TitleActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
+        res = getInstrumentation().getTargetContext().getResources();
+    }
+
+    public void testStart() throws Exception {
+        solo.assertCurrentActivity(errMsg, TitleActivity.class);
+    }
+
+    public void testStartButtonShouldGoToDebugActivity() {
+        solo.clickOnText(res.getString(R.string.start_button_text));
+        solo.assertCurrentActivity(errMsg, DebugActivity.class);
+    }
+
+    public void testRulesButtonShouldGoToRulesActivity() {
+        solo.clickOnText(res.getString(R.string.rules_button_text));
+        solo.assertCurrentActivity(errMsg, RulesActivity.class);
+    }
+
+    public void testAboutButtonShouldGoToAboutActivity() {
+        solo.clickOnText(res.getString(R.string.about_button_text));
+        solo.assertCurrentActivity(errMsg, AboutActivity.class);
+    }
+
+    public void testCreditsButtonShouldGoToCreditsActivity() {
+        solo.clickOnText(res.getString(R.string.credits_button_text));
+        solo.assertCurrentActivity(errMsg, CreditsActivity.class);
+    }
+
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
+}
