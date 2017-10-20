8c14de98cb70871b2ac99253807589d34019c127
nnguyen01 <Nathan.Ngn10@gmail.com>
2017-10-20 02:43:16 -0600

    Added documentation to QuestionController

--- app/src/main/java/com/cmput401f17/eplscavengerhunt/controller/QuestionController.java
+++ app/src/main/java/com/cmput401f17/eplscavengerhunt/controller/QuestionController.java
@@ -6,12 +6,13 @@ import com.cmput401f17.eplscavengerhunt.ScavengerHuntApplication;
 import com.cmput401f17.eplscavengerhunt.model.Question;
 import com.cmput401f17.eplscavengerhunt.model.Response;
 import com.cmput401f17.eplscavengerhunt.model.ScavHuntState;
-import com.cmput401f17.eplscavengerhunt.model.Zone;
+
 import javax.inject.Inject;
 
 
 /**
- * An intermediate class that passes data
+ * QuestionController handles obtaining a question,
+ * passing the users answer to be stored and skipping a question
  */
 public class QuestionController {
 
@@ -24,7 +25,8 @@ public class QuestionController {
 
     /**
      * Gets the data relating to the current question
-     * @return A Question object relating to the current question
+     * @return Question     A question object containing a prompt and a solution and
+     *                      other specifics depending on the type of question
      */
     public Question requestQuestion() {
 
@@ -33,15 +35,18 @@ public class QuestionController {
 
 
     /**
-     *  Passes/ updates the user answer
-     *  @pre User has input an answer
-     *  @param answer
+     *  Updates the user's answer for the current question
+     *  @param answer       The users answer to the current question
      */
     public void requestSubmitResponse(String answer){
         Response response = new Response(answer);
         scavHuntState.addResponse(response);
-
     }
+
+    /**
+     * Skips the current question
+     * @param question      The question currently displayed to the user
+     */
     public void skip(Question question){
         question.skip();
     }
