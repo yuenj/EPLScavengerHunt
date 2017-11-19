f9270c6f15e504d215c5c82805356e463a9a8943
nnguyen01 <Nathan.Ngn10@gmail.com>
2017-11-19 14:53:49 -0700

    Fixed issue #154 - Prevented screen dimming

--- app/src/main/java/com/cmput401f17/eplscavengerhunt/activity/QuestionAnswerActivity.java
+++ app/src/main/java/com/cmput401f17/eplscavengerhunt/activity/QuestionAnswerActivity.java
@@ -5,7 +5,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
