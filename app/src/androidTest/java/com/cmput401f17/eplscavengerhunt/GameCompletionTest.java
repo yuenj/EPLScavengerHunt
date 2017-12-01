aec65d696518076eacdbaf78040c391064ab74a9
ry-prz <rperez@ualberta.ca>
2017-12-01 12:34:50 -0700

    removed import statements of debugactivity (#236)

--- app/src/androidTest/java/com/cmput401f17/eplscavengerhunt/GameCompletionTest.java
+++ app/src/androidTest/java/com/cmput401f17/eplscavengerhunt/GameCompletionTest.java
@@ -19,39 +19,6 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
-import com.cmput401f17.eplscavengerhunt.activity.AboutActivity;
-import com.cmput401f17.eplscavengerhunt.activity.CreditsActivity;
-import com.cmput401f17.eplscavengerhunt.activity.DebugActivity;
-import com.cmput401f17.eplscavengerhunt.activity.LocationActivity;
-import com.cmput401f17.eplscavengerhunt.activity.RulesActivity;
-import com.cmput401f17.eplscavengerhunt.activity.TitleActivity;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import android.support.test.espresso.IdlingPolicies;
-import android.support.test.espresso.IdlingResource;
-import android.support.test.espresso.intent.Intents;
-import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import android.text.format.DateUtils;
-
-import java.util.concurrent.TimeUnit;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.pressBack;
-import static android.support.test.espresso.Espresso.registerIdlingResources;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-
 
 /**
  * Instrumentation test that tests game completion use cases (UC 6,7), while also
